8b28b1aadd2abf83b62dbea3badbcb6d3e0469e0
abdiwali96 <abdiwali96@gmail.com>
2020-09-25 17:42:26 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Abdiwali Abdi on 25/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
